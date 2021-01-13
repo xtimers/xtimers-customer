4494e3b63b2735e21c682de909707f547b9df78e
Vijay Kumar Rajput <vijaykrajpoot@gmail.com>
2021-01-12 23:47:10 -0800

    Added Opena API

--- xtimers-customer-server/src/main/java/com/xtimers/customer/server/XtimersCustomerServer.java
+++ xtimers-customer-server/src/main/java/com/xtimers/customer/server/XtimersCustomerServer.java
@@ -1,6 +1,7 @@
 package com.xtimers.customer.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.env.PropertiesPropertySource;
